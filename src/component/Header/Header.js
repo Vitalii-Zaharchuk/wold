395f6123977725d5bad0ee814b49f0b2b17bd493
Vitalii-Zaharchuk <vitaliy.zakharchuk19@gmail.com>
2020-06-30 11:14:52 +0300

    Header

---
+++ src/component/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import s from './Header.module.css'
+let Header = (props) =>{
+    return(
+        <div className ={s.header}>
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcQrPotIEiUTbNLUNoJE5gAdjdirbVcFSvLFvg&usqp=CAU'/>
+        </div>
+    )
+}
+export default Header;
